1857fe7530a56cd43d0c8be4c27ba3a3853498cc
AuctorisMors <GitHub@Baerthe.com>
2026-01-01 19:54:25 -0800

    Refactor Ball class; add VisibleOnScreenNotifier2D and implement ResetBall method for improved ball reset logic

--- scripts/pong/Ball.cs
+++ scripts/pong/Ball.cs
@@ -10,18 +10,19 @@ public sealed partial class Ball : CharacterBody2D
     private CollisionShape2D _collisionShape;
     private ColorRect _colorRect;
     private Vector2 _initialPosition;
+    private VisibleOnScreenNotifier2D _visibleNotifier;
     private float _speedFactor;
     public override void _Ready()
     {
         _collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
         _colorRect = GetNode<ColorRect>("ColorRect");
+        _visibleNotifier = GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
         _collisionShape.Shape = new CircleShape2D() { Radius = Size / 2 };
         _colorRect.Size = new Vector2(Size, Size);
         _colorRect.Position = new Vector2(-Size / 2, -Size / 2);
         _initialPosition = GlobalPosition;
-        _speedFactor = 1.0f / Acceleration;
-        // Launch the ball in a random direction for testing
-        Velocity = new Vector2(-8000, 0);
+        _visibleNotifier.Connect("screen_exited", new Callable(this, nameof(ResetBall)));
+        ResetBall();
     }
     public override void _PhysicsProcess(double delta)
     {
@@ -36,5 +37,16 @@ public sealed partial class Ball : CharacterBody2D
                 Velocity = new Vector2(Velocity.X, GD.RandRange(-512, 512));
             }
         }
+    }
+    private void ResetBall()
+    {
+        Velocity = Vector2.Zero;
+        GlobalPosition = _initialPosition;
+        _speedFactor = 1.0f / Acceleration;
+        var flip = GD.Randf() < 0.5f ? -1 : 1;
+        if (flip < 0)
+            Velocity = new Vector2( -8000, GD.RandRange(-512, 512));
+        else
+            Velocity = new Vector2( 8000, GD.RandRange(-512, 512));
     }
 }
